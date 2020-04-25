2800237f43f0e71c137d7ff8365ccf6fc57b9fe2
GabrielBalzer <gbalzer96@gmail.com>
2020-04-25 14:35:42 +0200

    Decode commands with full bits MIC-4

--- microsim/FileHandling.cs
+++ microsim/FileHandling.cs
@@ -11,27 +11,82 @@ namespace microsim
 {
     class FileHandling
     {
-        public string test;
-        
+        public class Command
+        {
+            public string command { get; set; }
+            public uint data { get; set; }
+        }
 
         public void Filehandlingfunc()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "LST Dateien (*.lst) |*.LST";
+            openFileDialog.Filter = "LST Dateien (*.LST) |*.LST";
             openFileDialog.ShowDialog();
-            test = File.ReadAllText(openFileDialog.FileName);
             List<string> file_list = new List<string>();
+            var commandList = new List<Command>();
             foreach(string line in File.ReadAllLines(openFileDialog.FileName))
             {
                 if (!string.IsNullOrEmpty(line))
                 {
                     file_list.Add(line);
+                 
+                    if(!string.IsNullOrWhiteSpace(line.Substring(5, 4)))
+                    {
+                        commandList.Add(new Command
+                        {
+                            command = line.Substring(5, 4),
+                            data = 0
+                        }) ;
+                    }
                 }
             }
-            foreach(string item in file_list)
+            CommandDecoder(commandList);
+        }
+
+        public void CommandDecoder(List<Command> commandList)
+        {
+            uint hex;
+            foreach(var item in commandList)
             {
-                Console.WriteLine(item);
+
+                hex = Convert.ToUInt32(item.command, 16);
+
+                switch(hex)
+                {
+                    case 0b0000000001100100:
+                        item.command = "CLRWDT";
+                        break;
+                    case 0b0000000000001001:
+                        item.command = "RETFIE";
+                        break;
+                    case 0b0000000000001000:
+                        item.command = "RETURN";
+                        break;
+                    case 0b0000000001100011:
+                        item.command = "SLEEP";
+                        break;
+                    case 0b0000000000000000:
+                        item.command = "NOP";
+                        break;
+                    case 0b0000000001000000:
+                        item.command = "NOP";
+                        break;
+                    case 0b0000000000100000:
+                        item.command = "NOP";
+                        break;
+                    case 0b0000000001100000:
+                        item.command = "NOP";
+                        break;
+                    default:
+                        break;
+                }
+
+
+                Console.WriteLine(item.command);
+                Console.WriteLine(item.data);
             }
+
+
         }
     }
 }
