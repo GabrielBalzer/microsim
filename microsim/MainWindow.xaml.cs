c6e22b95e5da12a739a1d217a5ed740ba16f42b8
GabrielBalzer <gbalzer96@gmail.com>
2020-04-27 17:20:03 +0200

    Add Mahapp Retro Resources

--- microsim/MainWindow.xaml.cs
+++ microsim/MainWindow.xaml.cs
@@ -14,13 +14,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using MahApps.Metro.Controls;
 
 namespace microsim
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         FileHandling FileHandlingLocal = new FileHandling();
         CommandDecoder CommandDecoder = new CommandDecoder();
